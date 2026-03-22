4037c846b3903b850d23a546a2cdf3e2281f5ed2
north238 <i.make.a.studious.effort@gmail.com>
2026-03-22 10:24:04 +0900

    feat: add initial server implementation and configuration

---
+++ cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "GoWatch server is running")
+	})
+
+	log.Println("Server starting on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
